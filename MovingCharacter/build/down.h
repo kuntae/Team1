afce0828f674c3d1571b8999f817e4d31d1f88bd
hansung <hansung@hansung-VirtualBox.(none)>
2012-12-17 21:05:43 +0900

    MovingCode

---
+++ MovingCharacter/build/down.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(down)
+
+//======================================================================
+//
+//	down, 256x192@8, 
+//	+ palette 256 entries, not compressed
+//	+ 768 tiles Metatiled by 4x4 not compressed
+//	Total size: 512 + 49152 = 49664
+//
+//	Time-stamp: 2012-12-17, 21:03:09
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.10
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_DOWN_H
+#define GRIT_DOWN_H
+
+#define downTilesLen 49152
+extern const unsigned int downTiles[12288];
+
+#define downPalLen 512
+extern const unsigned short downPal[256];
+
+#endif // GRIT_DOWN_H
+
+//}}BLOCK(down)
---
+++ MovingCharacter/build/up.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(up)
+
+//======================================================================
+//
+//	up, 256x192@8, 
+//	+ palette 256 entries, not compressed
+//	+ 768 tiles Metatiled by 4x4 not compressed
+//	Total size: 512 + 49152 = 49664
+//
+//	Time-stamp: 2012-12-17, 21:03:10
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.10
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_UP_H
+#define GRIT_UP_H
+
+#define upTilesLen 49152
+extern const unsigned int upTiles[12288];
+
+#define upPalLen 512
+extern const unsigned short upPal[256];
+
+#endif // GRIT_UP_H
+
+//}}BLOCK(up)
